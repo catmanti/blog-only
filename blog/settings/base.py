a249565e0b0d0a4e6d70203f6924fec49575931b
catmanti <somaratne@yahoo.com>
2022-05-05 07:09:28 +0530

    Add Custom Menus as Carls Tutorial

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'home',
     'search',
     'streams',
+    'menus',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
---
+++ menus/templatetags/nav_tags.py
@@ -0,0 +1,20 @@
+from django import template
+from wagtail.core.models import Site
+from ..models import Menu
+
+register = template.Library()
+
+@register.simple_tag()
+def get_navbar(slug):
+    return Menu.objects.get(slug=slug) 
+
+# @register.inclusion_tag("tags/nav_bar.html")
+# def get_navbar():
+#     site= Site.objects.get(is_default_site=True)
+#     home_page=site.root_page
+#     pages=home_page.get_children().live().in_menu()
+
+
+#     return {
+#         "pages":pages,
+#     }
